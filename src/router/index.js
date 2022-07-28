fa6059ad9a4ddbfd1139c52bc797585aab649b03
hungpv <phanhung2702@gmail.com>
2022-07-28 17:03:02 +0700

    [Add] add ui draft component

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,20 @@
 import { createRouter, createWebHistory } from "vue-router";
+import Home from "../views/Home.vue";
+import Signin from "../views/Signin.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
+    {
+      path: "/",
+      name: "home",
+      component: Home,
+    },
+    {
+      path: "/signin",
+      name: "signin",
+      component: Signin,
+    },
   ],
 });
 
